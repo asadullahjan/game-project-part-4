ff1b4b8e0a0bbc3b5e7c740efd3111deb9daed7c
asadullahjan <asadullah.jan@outlook.com>
2022-06-05 11:21:04 +0500

    added canyon and collectable functions

--- sketch.js
+++ sketch.js
@@ -16,10 +16,14 @@ var scrollPos;
 var clouds;
 var mountains;
 var trees;
+var canyons;
+var collectables;
+
+
 var tressAnchorX = 270;
 var cloudAnchorX = 150;
 var mountainAnchorX = 150;
-var canyonAnchorX = 270;
+var canyonAnchorX = 100;
 var collactableAnchorX = 270;
 
 function setup() {
@@ -49,7 +53,7 @@ function setup() {
 
 	clouds = [
 		{
-			posX: 120,
+			posX: cloudAnchorX,
 			posY : random ( 50 , 150),
 			scale: random(1.0 , 1.5)
 		},
@@ -72,7 +76,7 @@ function setup() {
 
 	mountains = [
 		{
-			posX: 150,
+			posX: mountainAnchorX,
 			posY: 256
 		},
 		{
@@ -84,6 +88,40 @@ function setup() {
 			posY: 256
 		}
 	]
+
+	canyons = [
+		{
+			posX : canyonAnchorX,
+			width : random(50,100)
+		},
+		{
+			posX : canyonAnchorX+340,
+			width : random(50,100)
+		},
+		{
+			posX : canyonAnchorX+770,
+			width : random(50,100)
+		}
+		
+	];
+
+	collectables = [
+		{
+			posX: collactableAnchorX,
+			posY: random(floorPos_y,floorPos_y-50),
+			size: random(20,40)
+		},
+		{
+			posX: collactableAnchorX + 300,
+			posY: random(floorPos_y,floorPos_y-50),
+			size: random(20,40)
+		},
+		{
+			posX: collactableAnchorX +450,
+			posY: random(floorPos_y,floorPos_y-50),
+			size: random(20,40)
+		}
+	];
 	// Boolean variables to control the movement of the game character.
 	isLeft = false;
 	isRight = false;
@@ -98,21 +136,26 @@ function draw() {
 	background(100, 155, 255); // fill the sky blue
 
 
-	// Draw mountains.
+	////////// Draw mountains.
 	mountain();//calls the mountain function.
-	// Draw trees.
+	
+	////////// Draw trees.
 	tree();
-	// draw some green ground
+	
+	////////// draw some green ground
 	noStroke();
 	fill(0, 155, 0);
 	rect(0, floorPos_y, width, height / 4);
-	// Draw clouds.
+	
+	////////// Draw clouds.
 	clound();//calls the clound function.
 	
 
-	// Draw canyons
+	////////// Draw canyons
+	canyon();
 
-	// Draw collectable items
+	///////// Draw collectable items
+	collectable();
 
 	// Draw the game character - this must be last
 	gameCharacter();
@@ -242,5 +285,27 @@ function mountain() {
 					mountains[i].posX - 53, mountains[i].posY + 94, 
 					mountains[i].posX + 53, mountains[i].posY + 94
 				);
+	}
+}
+function canyon() {
+	for(var i = 0 ; i < canyons.length ; i++){
+		noStroke();
+		fill(48,25,52);
+		rect(canyons[i].posX,floorPos_y,canyons[i].width,200);
+		fill(15,94,156);
+		rect(canyons[i].posX,floorPos_y+100,canyons[i].width,100);
+	}
+}
+
+function collectable() {
+	for(var i = 0 ; i<collectables.length ; i++){
+		strokeWeight(2);
+		stroke(0,255,0);
+		line(collectables[i].posX+3,collectables[i].posY,collectables[i].posX+7,collectables[i].posY-collectables[i].size);
+		noStroke();
+		fill(255,0,0);
+		ellipse(collectables[i].posX,collectables[i].posY+5,collectables[i].size/2,collectables[i].size);
+		ellipse(collectables[i].posX+7,collectables[i].posY+5,collectables[i].size/2,collectables[i].size);
+
 	}
 }
